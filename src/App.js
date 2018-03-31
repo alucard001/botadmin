5b95e138c9f6874ab682edf0814f5b3daf5f2a3c
Ellery <alucard001@gmail.com>
2018-03-31 13:57:28 +0800

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
 // import InputRow from './components/InputRow';
 
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
-	state = {
-		intentOptions:[
+
+	constructor(props) {
+		super(props);
+
+		let intentOptions = [
 			{
 				name: "Opt 1",
 				value: "opt_1"
-			},
-			{
+			}, {
 				name: "Opt 2",
 				value: "opt_2"
 			}
-		]
+		];
+
+		this.state = {
+			intentOptions: intentOptions,
+			utterance:'',
+			intent:'',
+		};
+	}
+
+	submitToLUIS = () => {
+		console.log("Submit to LUIS: ", this.state);
 	};
 
+	setIntent = (e) => {
+		this.setState({intent: e.target.value});
+	};
+
+	setUtterance = (e) => {
+		this.setState({utterance: e.target.value});
+	};
+
+	componentDidUpdate = (prevProps, prevState) => {
+		// this.submitToLUIS();
+	}
+
+	shouldComponentUpdate = (nextProps, nextState) => {
+		return true;
+	}
 
 	render() {
 		return (
@@ -31,18 +56,18 @@ class App extends Component {
 					</div>
 					<div className="row">
 						<div className="col-sm-4">
-							<input className="form-control utterance" type="text" />
+							<input className="form-control utterance" type="text" onChange={this.setUtterance} />
 						</div>
 						<div className="col-sm-4">
-							<select className="form-control intent">
-								{
-								this.state.intentOptions.map(({name, value}, i) =>
+							<select className="form-control intent" onChange={this.setIntent}>
+								<option key="intent_defualt" value="">Select...</option>
+								{this.state.intentOptions.map(({name, value}, i) =>
 									<option key={value} value={value}>{name}</option>
 								)}
 							</select>
 						</div>
 						<div className="col-sm-4">
-							<button className="btn btn-primary">Create</button>
+							<button className="btn btn-primary" onClick={this.submitToLUIS}>Create</button>
 						</div>
 					</div>
 				</div>
